2f61dabd527673ae7c624485c39bed6420fbc218
Harpuia <marcelo.cardenas@usach.cl>
2023-09-17 16:37:11 -0300

    wip: tailwind integrado

---
+++ src/components/HeaderDashboard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template lang="">
+    <div>
+        <h1>Esto es un header</h1>
+    </div>
+</template>
+
+<style scoped>
+    h1 {
+        color: violet;
+    }
+    div {
+        background-color: black;
+    }
+</style>
---
+++ src/components/LeftMenuDashboard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template lang="">
+    <div>
+        <h2>Esto es el menu</h2>
+        <div class="text-red-700 text-xl">Hello, from TailwindCSS!</div>
+    </div>
+</template>
+
+<style scoped>
+    h2 {
+        color: red;
+    }
+    div {
+        background-color: green;
+    }
+</style>
